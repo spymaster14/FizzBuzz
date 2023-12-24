6ad5339d060831fbbeeb7a0af4bfd6a45c500235
HiroyukiYoneda <148459045+spymaster14@users.noreply.github.com>
2023-12-24 12:07:31 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,8 +2,18 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		System.out.println("FizzBuzz");
-
+		int number = 0;
+		while (number < 100) {
+			number++;
+			if (number % 15 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (number % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (number % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(number);
+			}
+		}
 	}
-
 }
